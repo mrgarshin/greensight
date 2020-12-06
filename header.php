8ed1f5bad1ea3133b7b876cbb84b157a680e39b7
Sergey Garshin <74963806+mrgarshin@users.noreply.github.com>
2020-12-06 12:42:55 +0300

    Update header.php

--- header.php
+++ header.php
@@ -9,32 +9,6 @@
         form input {
             margin: 4px;
         }
-        .modal-container {
-            display: none;
-        }
-        .modal-window {
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            min-width: 50%;
-            z-index: 1000;
-            opacity: 0;
-            transition: all 1s ease;
-        }
-        .modal-dialog {
-            box-shadow: 0 0 15px 0;
-        }
-        .modal-background {
-            z-index: 100;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            height: 0;
-            background-color: rgba(0, 0, 0, .7);
-            transition: all 1s ease;
-        }
         </style>
     </head>
     <body>
